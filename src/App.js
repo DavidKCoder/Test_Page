59261ff1a829b76e25c4029d2a5afde8411e024e
DavidKizinger <karapetyand91@gmail.com>
2020-02-13 02:08:43 +0400

    chore(test):refactor:fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Slider from "./components/Slider/Slider";
+// import Slider from "./components/Slider/Slider";
 import MyBlock from "./components/MyBlock/MyBlock";
 import Footer from "./components/Footer/Footer";
 import { Route, BrowserRouter} from "react-router-dom";
 import Contact from "./components/Contact/Contact";
 import Blog from "./components/Blog/Blog";
+import Article from "./components/Article/Article";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="container">
         <Header />
-        <Route path="/about" component={Slider} />
-        <Route path="/about" component={MyBlock} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/article" component={Article} />
         <Route path="/blog" component={Blog} />
+        <Route path="/blog" component={MyBlock} />
+        <Route path="/contact" component={Contact} />
+        {/* <Route path="/article" component={} /> */}
         <Footer />
       </div>
     </BrowserRouter>
